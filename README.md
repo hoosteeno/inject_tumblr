112bd5e6184741915f898bfc0634bde3ca10576f
hoosteeno <justin@dojo4.com>
2012-05-09 11:07:23 -0600

    adding code and readme

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-inject_tumblr
-=============
+# inject_tumblr
+
+## A jQuery plugin for getting some Tumblr posts and putting them on your site
+
+### Instructions and Caveats
+
+1) Get [mustache.js](https://github.com/janl/mustache.js) and inject-tumblr.js and put in your web directory.
+1) Include both in a script tag:
+    <script src="/javascripts/mustache.js" type="text/javascript"></script>
+    <script src="/javascripts/inject_tumblr.js" type="text/javascript"></script>
+1) Call inject_tumblr on a set of jQuery elements:
+    <script>
+      jq('#tumblr').inject_tumblr({
+        tumblr_id: 'theonlymagicleftisart',
+        data: {
+          api_key: '3VCeCOG7JTnON9cxj8euDHyLHkMdQVy3tpG289S1IND6WhAv0A'
+        }
+      });
+    </script>
+1) Optional and recommended! Provide your own templates for rendering the output:
+    <script>
+      jq('#tumblr').inject_tumblr({
+        tumblr_id: 'scottjurek',
+        down_template: jq('script.template[name="tumblr-down-template"]').first().html(),
+        post_template: jq('script.template[name="tumblr-post-template"]').first().html(),
+        data: {
+          api_key: '3VCeCOG7JTnON9cxj8euDHyLHkMdQVy3tpG289S1IND6WhAv0A'
+        }
+      });
+    </script>
+1) Optional! Make a cool pinwheel graphic or prancing unicorn that will display while the plugin gets posts from Tumblr, and then vanish like magic when it has loaded:
+    <script>
+      jq('#tumblr').inject_tumblr({
+        tumblr_id: 'scottjurek',
+        loading_msg: jq('#tumblr .loading'),
+        data: {
+          api_key: '3VCeCOG7JTnON9cxj8euDHyLHkMdQVy3tpG289S1IND6WhAv0A'
+        }
+      });
+    </script>
+1) A fairly complete set of configurable parameters are below, with their defaults:
+    jq.fn.inject_tumblr.default_options = {¶
+      timeout: 5000,¶
+      templatize: true,¶
+      tumblr_id: 'dojo4',¶
+      loading_msg: '',¶
+      post_template: jq.fn.inject_tumblr.post_template,¶
+      down_template: jq.fn.inject_tumblr.down_template,¶
+      no_posts_template: jq.fn.inject_tumblr.no_posts_template,¶
+      post_type: 'text',¶
+      data: {¶
+        api_key: '',¶
+        tag: '',¶
+        limit: 10,¶
+        filter: ''¶
+      }¶
+    }
+1) There are certainly bugs, since it's my first jQuery plugin. What a great opportunity for us to learn from one another!
---
+++ inject_tumblr.js
@@ -0,0 +1,157 @@
+(function(jq){  
+
+  jq.fn.inject_tumblr = function(options) {
+
+    var required = [
+      'api_key',
+      'tumblr_id'
+    ]
+    for (i=0; i<required.length; i++) {
+      if (options[required[i]] == null && options.data[required[i]] == null) {
+        throw new Error('inject_tumblr: ' + required[i] + ' is required');
+      }
+    }
+
+    options = jq.extend({}, jq.fn.inject_tumblr.default_options, options);
+
+    var error = false;
+
+    var enforce_timeout = window.setTimeout(function (){
+      generate_error(options.down_template);
+    }, options.timeout);
+
+    var get_posts = function() {
+      jq.ajax({
+        url: 'http://api.tumblr.com/v2/blog/'+options.tumblr_id+'.tumblr.com/posts',
+        //crossDomain: true,
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        context: this,
+        success: function(data) {
+          handle_response(data);
+        },
+        data: options.data
+      });
+    }
+
+    enhance_post = function(post) {
+
+      post.date = new Date(parseInt(post.timestamp*1000));
+      post.iso_date = post.date.toISOString();
+      post.human_date = post.date.toDateString();
+      post.year = post.date.getFullYear();
+    
+      if (options.post_type == 'text') {
+        post.body = '<div>'+post.body+'</div>';
+
+        post.first_picture = (function(){
+          return jq(post.body).find('img').first().attr('src') || 'images/blog_thumb_default.jpg';
+        })();
+
+        post.blurb = (function(){
+          var blurb = document.createElement('div');
+          jq(blurb).append(jq(post.body).find('p').slice(0, 2));
+          return jq(blurb).html();
+        })();
+      }
+
+      if (options.post_type == 'link') {
+        post.body = '<div>'+post.description+'</div>';
+        post.blurb = (function(){
+          var blurb = document.createElement('div');
+          jq(blurb).append(jq(post.body).find('p').slice(0, 2));
+          return jq(blurb).html();
+        })();
+      }
+      
+      return post;
+    }
+
+    var generate_error = function(template) {
+      if (! error) {
+
+        if (options.loading_msg != null) {
+          jq(options.loading_msg).remove();
+        }
+
+        error = true;
+        element.append(Mustache.render(template, ""));
+      }
+    }
+
+    var element = this;
+    var posts = [];
+
+    get_posts();
+
+    var handle_response = function(result){
+
+      if (! error) {
+
+        if (result.meta.status != 200) {
+          generate_error(options.down_template);
+        }
+
+        else if (result.response.posts.length == 0) {
+          generate_error(options.no_posts_template);
+        }
+
+        else {
+
+          window.clearTimeout(enforce_timeout);
+
+          if (options.loading_msg != null) {
+            jq(options.loading_msg).remove();
+          }
+
+          posts = result.response.posts;
+
+          jq(posts).map(function() { 
+            var post = this;
+
+            if (options.post_type != null && post.type != options.post_type) {
+              return null;
+            }
+
+            post = enhance_post(post);
+
+            if (options.templatize == true) {
+              post = Mustache.render(options.post_template, post);
+            }
+
+            element.append(post);
+
+            return post;
+          });
+        }
+      }
+    };
+
+    return element;
+
+  }
+
+  jq.fn.inject_tumblr.post_template = '<article><figure><a href="{{post_url}}"><img src="{{first_picture}}"></a></figure><div><h3><a href="{{post_url}}">{{title}}</a></h3><time datetime="{{iso_date}}">{{human_date}}</time>{{{blurb}}}<a href="{{post_url}}">More &raquo;</a></div></article>';
+  jq.fn.inject_tumblr.down_template = '<article><h3>Uh oh..</h3><div>It looks like tumblr is down right now. Please refresh your browser window in a little while.</div></article>';
+  jq.fn.inject_tumblr.no_posts_template = '<article><h3>No posts yet!</h3><div>This blog is empty right now. Please check again soon!</div></article>';
+
+  jq.fn.inject_tumblr.default_options = {
+    timeout: 5000,
+    templatize: true,
+    tumblr_id: 'dojo4',
+    loading_msg: '',
+    post_template: jq.fn.inject_tumblr.post_template,
+    down_template: jq.fn.inject_tumblr.down_template,
+    no_posts_template: jq.fn.inject_tumblr.no_posts_template,
+    post_type: 'text',
+    data: {
+      api_key: '',
+      tag: '',
+      limit: 10,
+      filter: ''
+    }
+  }
+
+  
+
+})(jQuery);
